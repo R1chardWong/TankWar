6f3935925d48648b1246cc00badef0e6590b4233
0kun1 <95329125+0kun1@users.noreply.github.com>
2022-06-28 19:30:20 +0800

    Update enemy.cpp

--- enemy.cpp
+++ enemy.cpp
@@ -2,10 +2,12 @@
 
 Enemy::Enemy()
 {
+    //设初速度和初始方向
     dir = down;
     speed = 6;
     rect.setRect(0*BASESIZE,0*BASESIZE,SIZE,SIZE);
 
+    //加载
     upimg1.load((rootdir+"\\pic\\gray-tank\\1-2-1.gif").c_str());
     upimg1 = resizePic(upimg1,SIZE,SIZE);
     upimg2.load((rootdir+"\\pic\\gray-tank\\1-2-2.gif").c_str());
@@ -28,6 +30,7 @@ Enemy::Enemy()
 
 }
 
+//赋值重定义
 bool Enemy::operator==(const Enemy &other)
 {
     if(this->rect==other.rect)//矩形框相等视为同一个对象
